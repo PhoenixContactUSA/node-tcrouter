9ce345781c28fac5fdf1886734ec513d25d8d706
zmink <zmink@phoenixcon.com>
2019-09-20 15:03:10 -0700

    Fixed router io tests

--- test/RouterIO.test.js
+++ test/RouterIO.test.js
@@ -3,133 +3,96 @@ var expect    = require('chai').expect;
 
 const net = require('net');
 const RouterIO = require('../src/RouterIO.js');
-const mockRouter = net.createServer();
 
 const DEVICE = {ip: '192.168.1.1',port:1432};
 const MOCK_DEVICE = {ip:'127.0.0.1',port: 6784};
 
-const USE_DEVICE = true;
+const USE_DEVICE = false;
 
 const TARGET = (USE_DEVICE===true) ? DEVICE:MOCK_DEVICE;
 
 describe('RouterIO test cases', function(){
 
-    // afterEach(function() {
-    //     mockRouter.close();
-    // });
-
-    it('Sends an accurately formed IO message to the TC Router Socket',function(){
+    it('Sends an accurately formed control output message to the TC Router Socket',function(){
         
-
+        const mockRouter = net.createServer();
         mockRouter.listen(MOCK_DEVICE.port,MOCK_DEVICE.ip,function(){
-            //TC_Router.sendSMS(message);
+            
             var IOController = new RouterIO(TARGET.port,TARGET.ip,3000);
             IOController.controlOutput(1,true).then((success)=>{
                 console.log(success);
             }).catch((e)=>{
-                console.log(e);
+                expect.fail();
             })
         });
         mockRouter.on('connection',function(socket){
             socket.on('data',function(data){
-                //expect(data.toString()).to.equal(`<?xml version="1.0"?>\n<cmgs destaddr="+17176026963">This is the tcrouter dslink calling</cmgs>`)
+                expect(data.toString()).to.equal(`<?xml version="1.0"?><io><output no="1" value="on"/></io>`)
             })
         })
-        //TC_Router.sendSMS(message);
-
-
     })
 
     it('Sends an accurately formed IO status message to the TC Router Socket',function(){
         const mockRouter = net.createServer();
-        
-        //let message = new SMS('7176026963','Hello from tcrouter-node');
-        
 
         mockRouter.listen(MOCK_DEVICE.port,MOCK_DEVICE.ip,function(){
-            //TC_Router.sendSMS(message);
             var IOController = new RouterIO(TARGET.port,TARGET.ip,3000);
             IOController.getIO().then((success)=>{
                 console.log(success);
             }).catch((e)=>{
-                console.log(e);
+                expect.fail();
             })
         });
         mockRouter.on('connection',function(socket){
             socket.on('data',function(data){
-                //expect(data.toString()).to.equal(`<?xml version="1.0"?>\n<cmgs destaddr="+17176026963">This is the tcrouter dslink calling</cmgs>`)
+                expect(data.toString()).to.equal(`<?xml version="1.0"?><io><output no="1"/><input no="1"/><input no="2"/></io>`)
             })
         })
-        //TC_Router.sendSMS(message);
     })
 
 
     it('Can send multiple status messages',function(){
         const mockRouter = net.createServer();     
-        //let message = new SMS('7176026963','Hello from tcrouter-node');
         
 
         mockRouter.listen(MOCK_DEVICE.port,MOCK_DEVICE.ip,function(){
-            //TC_Router.sendSMS(message);
             var IOController = new RouterIO(TARGET.port,TARGET.ip,3000);
             IOController.getIO().then((success)=>{
                 console.log(success);
             }).catch((e)=>{
-                console.log(e);
+                expect.fail();
             })
             IOController.getIO().then((success)=>{
                 console.log(success);
             }).catch((e)=>{
-                console.log(e);
+                expect.fail();
             })
         });
         mockRouter.on('connection',function(socket){
             socket.on('data',function(data){
-                //expect(data.toString()).to.equal(`<?xml version="1.0"?>\n<cmgs destaddr="+17176026963">This is the tcrouter dslink calling</cmgs>`)
+                expect(data.toString()).to.equal(`<?xml version="1.0"?><io><output no="1"/><input no="1"/><input no="2"/></io>`)
             })
         })
-        //TC_Router.sendSMS(message);
     })
 
-    it('Controls GPRS',function(){
+    it('Can send a properly formed Control GPRS message',function(){
+        const mockRouter = net.createServer();   
+
         mockRouter.listen(MOCK_DEVICE.port,MOCK_DEVICE.ip,function(){
-            //TC_Router.sendSMS(message);
             var IOController = new RouterIO(TARGET.port,TARGET.ip,3000);
             IOController.controlGprs(true).then((success)=>{
                 console.log(success);
             }).catch((e)=>{
-                console.log(e);
+                expect.fail();
             })
         });
         mockRouter.on('connection',function(socket){
             socket.on('data',function(data){
-                //expect(data.toString()).to.equal(`<?xml version="1.0"?>\n<cmgs destaddr="+17176026963">This is the tcrouter dslink calling</cmgs>`)
+                expect(data.toString()).to.equal(`<?xml version="1.0"?><io><gprs value="on"/></io>`)
             })
         })
-        //TC_Router.sendSMS(message);
 
 
-    })
-
-    
-    it('Controls Output',function(){
-        mockRouter.listen(MOCK_DEVICE.port,MOCK_DEVICE.ip,function(){
-            //TC_Router.sendSMS(message);
-            var IOController = new RouterIO(TARGET.port,TARGET.ip,3000);
-            IOController.controlOutput(1,true).then((success)=>{
-                console.log(success);
-            }).catch((e)=>{
-                console.log(e);
-            })
-        });
-        mockRouter.on('connection',function(socket){
-            socket.on('data',function(data){
-                //expect(data.toString()).to.equal(`<?xml version="1.0"?>\n<cmgs destaddr="+17176026963">This is the tcrouter dslink calling</cmgs>`)
-            })
-        })
-        //TC_Router.sendSMS(message);
-
-
-    })
+    });
 
 });
