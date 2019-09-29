ee8fa4f4942d940dfa2b2e63fa5ba7b11c9af51f
zmink <zmink@phoenixcon.com>
2019-09-29 13:05:43 -0700

    Added test for getMutableInfo call

--- test/RouterInfo.test.js
+++ test/RouterInfo.test.js
@@ -18,13 +18,39 @@ describe('RouterInfo test cases', function(){
             infoController.getInfo().then((success)=>{
                 console.log(success);
             }).catch((e)=>{
+                expect.fail();
                 console.log(e);
+                done();
             })
         });
         mockRouter.on('connection',function(socket){
             socket.on('data',function(data){
                 console.log(data.toString());
                 expect(data.toString()).to.equal(`<?xml version="1.0"?><info>        <device/>        <radio/>        <inet/>        <io/>        </info>`)
+                done();
+            })
+        })
+
+    })
+
+    it('Mutable info requests only mutable info on the tcrouter',function(done){
+        const mockRouter = net.createServer();
+
+        mockRouter.listen(MOCK_DEVICE.port,MOCK_DEVICE.ip,function(){
+            //TC_Router.sendSMS(message);
+            var infoController = new RouterInfo(MOCK_DEVICE.port,MOCK_DEVICE.ip,3000);
+            infoController.getMutableInfo().then((success)=>{
+                console.log(success);
+            }).catch((e)=>{
+                expect.fail();
+                console.log(e);
+                done();
+            })
+        });
+        mockRouter.on('connection',function(socket){
+            socket.on('data',function(data){
+                console.log(data.toString());
+                expect(data.toString()).to.equal(`<?xml version="1.0"?><info>        <radio/>        <inet/>        <io/>        </info>`)
                 done();
             })
         })
