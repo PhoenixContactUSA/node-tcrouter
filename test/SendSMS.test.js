9254fca74e720102b3209e0d40e9871c34ae7558
zmink-pxc <zmink@phoenixcontact.com>
2020-01-24 19:59:45 -0800

    Fixed test problem with sms send - indiviual mock routers per test

--- test/SendSMS.test.js
+++ test/SendSMS.test.js
@@ -5,14 +5,14 @@ var expect    = require('chai').expect;
 const SMS     = require('../src/sms.js');
 const net = require('net');
 const SendSMS = require('../src/SendSMS');
-const mockRouter = net.createServer();
+
 
 const MOCK_DEVICE = {ip:'127.0.0.1',port: 6924};
 
 describe('SendSMS test cases', function(){
 
     it('Sends an accurately formed SMS message to the TC Router Socket',function(){
-
+        const mockRouter = net.createServer();
         
         let message = new SMS('2025550154','Hello from tcrouter-node');
 
@@ -33,11 +33,12 @@ describe('SendSMS test cases', function(){
     });
 
     it('Sends multiple messages given multiple contacts',function(done){
+        const mockRouter = net.createServer();
         var sentCount = 0;
         let message = new SMS('2025550154,2025550155','Hello from tcrouter-node');
 
-        mockRouter.listen(MOCK_DEVICE.port,MOCK_DEVICE.ip,function(){
-            var sms = new SendSMS(MOCK_DEVICE.port,MOCK_DEVICE.ip,3000,message);
+        mockRouter.listen(MOCK_DEVICE.port+1,MOCK_DEVICE.ip,function(){
+            var sms = new SendSMS(MOCK_DEVICE.port+1,MOCK_DEVICE.ip,3000,message);
             sms.send().then((success)=>{
                 console.log(success);
             }).catch((e)=>{
